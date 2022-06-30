72b37f8d3c3716f4024930bc345f5c65865876f7
nagarajan1986 <nagu.tcl@gmail.com>
2022-06-30 16:54:35 +0530

    new code added

--- src/org/sample/SampleGit.java
+++ src/org/sample/SampleGit.java
@@ -2,9 +2,17 @@ package org.sample;
 
 public class SampleGit {
 	
+	
+	private void test1() {
+		
+		System.out.println("New coding added here now");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("sample text");
+		System.out.println("New coding added here now");
 		
 	}
 
