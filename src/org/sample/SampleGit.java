3f49dc8cfd18b48c1469b9b5754443df4ffc2e69
nagarajan1986 <nagu.tcl@gmail.com>
2022-07-02 18:06:34 +0530

    new code done by Naga1

--- src/org/sample/SampleGit.java
+++ src/org/sample/SampleGit.java
@@ -2,6 +2,13 @@ package org.sample;
 
 public class SampleGit {
 	
+	
+	private void newInformation() {
+		
+		System.out.println("new method added for Naga1");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("sample text");
