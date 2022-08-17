7f6b81ee4f8d7d1f6641554f6bf0d41df4db5a94
nagarajan1986 <108337003+nagarajan1986@users.noreply.github.com>
2022-08-17 20:36:37 +0530

    Update SampleGit.java

--- src/org/sample/SampleGit.java
+++ src/org/sample/SampleGit.java
@@ -3,21 +3,21 @@ package org.sample;
 public class SampleGit {
 	
 	
-<<<<<<< HEAD
+
 	private void newInformation() {
 		
 		System.out.println("new method added for Naga1");
-=======
+
 	private void test1() {
 		
 		System.out.println("New coding added here now");
->>>>>>> cc2882e11f659823010d600339f738a7bbc29ed9
 
 	}
 	
 	public static void main(String[] args) {
 		
 		System.out.println("sample text");
+		System.out.println("New coding added here now");
 		System.out.println("New coding added here now");
 		
 	}
